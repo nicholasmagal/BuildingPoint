449f7b004130bf5728f34ac52e63825a88dee08b
nick <nim21@miami.edu>
2019-04-03 14:30:17 -0400

    Start of the camera consistently sending data

--- app/src/main/java/com/example/buildingpoint/MainActivity.java
+++ app/src/main/java/com/example/buildingpoint/MainActivity.java
@@ -28,7 +28,9 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.common.FirebaseMLException;
 import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
 import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.common.modeldownload.FirebaseRemoteModel;
 import com.google.firebase.ml.custom.FirebaseModelDataType;
 import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
 import com.google.firebase.ml.custom.FirebaseModelInputs;
@@ -38,8 +40,10 @@ import com.google.firebase.ml.custom.FirebaseModelOutputs;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener,Camera.PreviewCallback{
 
@@ -106,13 +110,129 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         byte[] bytes = out.toByteArray();
         final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
 
+
+
         MainActivity.this.runOnUiThread(new Runnable() {
 
             @Override
             public void run() {
                 //Connect the firebase model here
+                try{
+                    getTFModel(bitmap);
+                }
+                catch (Exception e){
+                    Log.i("FireBaseModel ","failed");
+                }
             }
         });
+    }
+
+    public void getTFModel(Bitmap bitmap) throws FirebaseMLException {
+        final TextView myResult = findViewById(R.id.result);
+        Log.i("SUCCESS", "-1");
+/*
+        //Getting the TensorFlowModel from assets folder
+
+        FirebaseLocalModel localSource =
+                new FirebaseLocalModel.Builder("my_local_model")  // Assign a name to this model
+                        .setAssetFilePath("model.tflite")
+                        .build();
+        FirebaseModelManager.getInstance().registerLocalModel(localSource);
+
+*/
+
+        //Getting the model from FireBase
+
+        FirebaseModelDownloadConditions.Builder conditionsBuilder =
+                new FirebaseModelDownloadConditions.Builder().requireWifi();
+
+        FirebaseModelDownloadConditions conditions = conditionsBuilder.build();
+
+
+        FirebaseRemoteModel cloudSource = new FirebaseRemoteModel.Builder("building-detector")
+                .enableModelUpdates(true)
+                .setInitialDownloadConditions(conditions)
+                .setUpdatesDownloadConditions(conditions)
+                .build();
+        FirebaseModelManager.getInstance().registerRemoteModel(cloudSource);
+        Log.i("SUCCESS", "0");
+
+
+        //Creating the interperter from the model
+
+        FirebaseModelOptions options = new FirebaseModelOptions.Builder()
+                .setRemoteModelName("building-detector").build();
+        FirebaseModelInterpreter firebaseInterpreter =
+                FirebaseModelInterpreter.getInstance(options);
+        Log.i("SUCCESS", "0.5");
+
+        //Process
+
+        FirebaseModelInputOutputOptions inputOutputOptions =
+                new FirebaseModelInputOutputOptions.Builder()
+                        .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 224, 224, 3})
+                        .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 4})
+                        .build();
+
+
+        Log.i("SUCCESS", "1");
+
+
+        //Performing Inference on Input data
+
+        bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+        int batchNum = 0;
+        float[][][][] input = new float[1][224][224][3];
+        for (int x = 0; x < 224; x++) {
+            for (int y = 0; y < 224; y++) {
+                int pixel = bitmap.getPixel(x, y);
+                // Normalize channel values to [0,1.0]
+                input[batchNum][x][y][0] = Color.red(pixel) / 255.0f;
+                input[batchNum][x][y][1] = Color.green(pixel) / 255.0f;
+                input[batchNum][x][y][2] = Color.blue(pixel) / 255.0f;
+            }
+        }
+        Log.i("SUCCESS", "2");
+        //Creating a input model
+        FirebaseModelInputs inputs = new FirebaseModelInputs.Builder()
+                .add(input)  // add() as many input arrays as your model requires
+                .build();
+
+        firebaseInterpreter.run(inputs, inputOutputOptions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseModelOutputs>() {
+                            @Override
+                            public void onSuccess(FirebaseModelOutputs result) {
+                                Log.i("SUCCESS", "4");
+                                float[][] output = result.getOutput(0);
+                                float[] probabilities = output[0];
+                                String[] labels={"Arts","Cox","Mcknighnight","Rainbow"};
+
+                                for (int i = 0; i < probabilities.length; i++) {
+                                    try {
+                                        BufferedReader reader = new BufferedReader(
+                                                new InputStreamReader(getAssets().open("train.txt")));
+                                        String label = reader.readLine();
+
+                                        Log.i("MLKit", String.format("%s: %1.4f", labels[i], probabilities[i]));
+                                    } catch (Exception e) {
+                                        Log.i("MLKIT", "FAIL");
+                                    }
+
+                                }
+
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.i("ERROR", " FAILURE AT THE END");
+                            }
+                        });
+        Log.i("SUCCESS", "3");
+
+
     }
 
     private void goToGallery() {
--- app/src/main/java/com/example/buildingpoint/ResultPage.java
+++ app/src/main/java/com/example/buildingpoint/ResultPage.java
@@ -36,13 +36,6 @@ public class ResultPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_page);
-        try {
-            Bitmap myBitMap = setImage();
-            getTFModel(myBitMap);
-        } catch (Exception e) {
-            Log.i("ERROR2", "The Bitmap could not be retrived");
-        }
-
     }
 
     public Bitmap setImage() throws Exception {
@@ -59,113 +52,7 @@ public class ResultPage extends AppCompatActivity {
         return pictureMap;
     }
 
-    public void getTFModel(Bitmap bitmap) throws FirebaseMLException {
-        final TextView myResult = findViewById(R.id.result);
-        Log.i("SUCCESS", "-1");
-/*
-        //Getting the TensorFlowModel from assets folder
 
-        FirebaseLocalModel localSource =
-                new FirebaseLocalModel.Builder("my_local_model")  // Assign a name to this model
-                        .setAssetFilePath("model.tflite")
-                        .build();
-        FirebaseModelManager.getInstance().registerLocalModel(localSource);
-
-*/
-
-        //Getting the model from FireBase
-
-        FirebaseModelDownloadConditions.Builder conditionsBuilder =
-                new FirebaseModelDownloadConditions.Builder().requireWifi();
-
-        FirebaseModelDownloadConditions conditions = conditionsBuilder.build();
-
-
-        FirebaseRemoteModel cloudSource = new FirebaseRemoteModel.Builder("building-detector")
-                .enableModelUpdates(true)
-                .setInitialDownloadConditions(conditions)
-                .setUpdatesDownloadConditions(conditions)
-                .build();
-        FirebaseModelManager.getInstance().registerRemoteModel(cloudSource);
-        Log.i("SUCCESS", "0");
-
-
-        //Creating the interperter from the model
-
-        FirebaseModelOptions options = new FirebaseModelOptions.Builder()
-                .setRemoteModelName("building-detector").build();
-        FirebaseModelInterpreter firebaseInterpreter =
-                FirebaseModelInterpreter.getInstance(options);
-        Log.i("SUCCESS", "0.5");
-
-        //Process
-
-        FirebaseModelInputOutputOptions inputOutputOptions =
-                new FirebaseModelInputOutputOptions.Builder()
-                        .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 224, 224, 3})
-                        .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 4})
-                        .build();
-
-
-        Log.i("SUCCESS", "1");
-
-
-        //Performing Inference on Input data
-
-        bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
-        int batchNum = 0;
-        float[][][][] input = new float[1][224][224][3];
-        for (int x = 0; x < 224; x++) {
-            for (int y = 0; y < 224; y++) {
-                int pixel = bitmap.getPixel(x, y);
-                // Normalize channel values to [0,1.0]
-                input[batchNum][x][y][0] = Color.red(pixel) / 255.0f;
-                input[batchNum][x][y][1] = Color.green(pixel) / 255.0f;
-                input[batchNum][x][y][2] = Color.blue(pixel) / 255.0f;
-            }
-        }
-        Log.i("SUCCESS", "2");
-        //Creating a input model
-        FirebaseModelInputs inputs = new FirebaseModelInputs.Builder()
-                .add(input)  // add() as many input arrays as your model requires
-                .build();
-
-        firebaseInterpreter.run(inputs, inputOutputOptions)
-                .addOnSuccessListener(
-                        new OnSuccessListener<FirebaseModelOutputs>() {
-                            @Override
-                            public void onSuccess(FirebaseModelOutputs result) {
-                                Log.i("SUCCESS", "4");
-                                float[][] output = result.getOutput(0);
-                                float[] probabilities = output[0];
-                                String[] labels={"Arts","Cox","Mcknighnight","Rainbow"};
-
-                                for (int i = 0; i < probabilities.length; i++) {
-                                    try {
-                                        BufferedReader reader = new BufferedReader(
-                                                new InputStreamReader(getAssets().open("train.txt")));
-                                        String label = reader.readLine();
-
-                                        Log.i("MLKit", String.format("%s: %1.4f", labels[i], probabilities[i]));
-                                    } catch (Exception e) {
-                                        Log.i("MLKIT", "FAIL");
-                                    }
-
-                                }
-
-                            }
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.i("ERROR", " FAILURE AT THE END");
-                            }
-                        });
-        Log.i("SUCCESS", "3");
-
-
-    }
 
 
 }
