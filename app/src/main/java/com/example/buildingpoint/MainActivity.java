b1526a2ae42d50c7effbc90ce627a5d654b11360
nick <nim21@miami.edu>
2019-04-04 10:16:58 -0400

    Second Method of just tapping screen

--- app/src/main/java/com/example/buildingpoint/MainActivity.java
+++ app/src/main/java/com/example/buildingpoint/MainActivity.java
@@ -47,10 +47,12 @@ import java.io.InputStreamReader;
 
 import android.os.AsyncTask;
 
-public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener,Camera.PreviewCallback{
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
 
     final int ACTIVITY_SELECT_PICTURE = 1;
     final int ACTIVITY_RESULT = 2;
+    private boolean cameraInUse;
+
 
     private Camera mCamera;
     private TextureView mTextureView;
@@ -65,9 +67,19 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         mTextureView = new TextureView(this);
         mTextureView.setSurfaceTextureListener(this);
 
+
         setContentView(mTextureView);
         mCamera = Camera.open();
-        mCamera.setPreviewCallback(this);
+        cameraInUse = false;
+
+        mTextureView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Tap", "U r clicking");
+            }
+        });
+
+        //mCamera.setPreviewCallback(this);
     }
 
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
@@ -97,42 +109,38 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
     }
 
     @Override
-    public void onPreviewFrame(byte[] data, Camera camera) {
-        Log.i("Camera","kkhhjh ");
+    protected void onResume() {
+        super.onResume();
+        if (cameraInUse) {
+            openCamera();
+        }
 
-        Camera.Parameters parameters = camera.getParameters();
-        int width = parameters.getPreviewSize().width;
-        int height = parameters.getPreviewSize().height;
-
-        YuvImage yuv = new YuvImage(data, parameters.getPreviewFormat(), width, height, null);
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        yuv.compressToJpeg(new Rect(0, 0, width, height), 50, out);
-
-        byte[] bytes = out.toByteArray();
-        final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-
-
-
-        MainActivity.this.runOnUiThread(new Runnable() {
-
-            @Override
-            public void run() {
-
-                try{
-                    Thread.sleep(2000);
-                    getTFModel(bitmap);
-                }
-                catch (Exception e){
-                    Log.i("FireBaseModel ","failed");
-                }
+        public void onActivityResult ( int requestCode, int resultCode, Intent data){
+            //Variables
+            ImageView displayedPicture = findViewById(R.id.preview_photo);
+            Uri selectedURI;
+            super.onActivityResult(requestCode, resultCode, data);
+            //If it works
+            switch (requestCode) {
+                case ACTIVITY_SELECT_PICTURE:
+                    if (resultCode == Activity.RESULT_OK) {
+                        selectedURI = data.getData();
+                        Intent resultIntent = new Intent();
+                        resultIntent.setClassName("com.example.buildingpoint",
+                                "com.example.buildingpoint.ResultPage");
+                        resultIntent.putExtra("theURI", selectedURI.toString());
+                        startActivityForResult(resultIntent, ACTIVITY_RESULT);
+                    }
+                    break;
+                default:
+                    Log.i("ERROR", "U DONE MESSED UP");
+                    break;
             }
-        });
-    }
+        }
 
-    public void getTFModel(Bitmap bitmap) throws FirebaseMLException {
-        final TextView myResult = findViewById(R.id.result);
-        Log.i("SUCCESS", "-1");
+        public void getTFModel (Bitmap bitmap) throws FirebaseMLException {
+            final TextView myResult = findViewById(R.id.result);
+            Log.i("SUCCESS", "-1");
 /*
         //Getting the TensorFlowModel from assets folder
 
@@ -144,129 +152,108 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
 
 */
 
-        //Getting the model from FireBase
+            //Getting the model from FireBase
 
-        FirebaseModelDownloadConditions.Builder conditionsBuilder =
-                new FirebaseModelDownloadConditions.Builder().requireWifi();
+            FirebaseModelDownloadConditions.Builder conditionsBuilder =
+                    new FirebaseModelDownloadConditions.Builder().requireWifi();
 
-        FirebaseModelDownloadConditions conditions = conditionsBuilder.build();
-
-
-        FirebaseRemoteModel cloudSource = new FirebaseRemoteModel.Builder("building-detector")
-                .enableModelUpdates(true)
-                .setInitialDownloadConditions(conditions)
-                .setUpdatesDownloadConditions(conditions)
-                .build();
-        FirebaseModelManager.getInstance().registerRemoteModel(cloudSource);
-        Log.i("SUCCESS", "0");
+            FirebaseModelDownloadConditions conditions = conditionsBuilder.build();
 
 
-        //Creating the interperter from the model
-
-        FirebaseModelOptions options = new FirebaseModelOptions.Builder()
-                .setRemoteModelName("building-detector").build();
-        FirebaseModelInterpreter firebaseInterpreter =
-                FirebaseModelInterpreter.getInstance(options);
-        Log.i("SUCCESS", "0.5");
-
-        //Process
-
-        FirebaseModelInputOutputOptions inputOutputOptions =
-                new FirebaseModelInputOutputOptions.Builder()
-                        .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 224, 224, 3})
-                        .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 4})
-                        .build();
+            FirebaseRemoteModel cloudSource = new FirebaseRemoteModel.Builder("building-detector")
+                    .enableModelUpdates(true)
+                    .setInitialDownloadConditions(conditions)
+                    .setUpdatesDownloadConditions(conditions)
+                    .build();
+            FirebaseModelManager.getInstance().registerRemoteModel(cloudSource);
+            Log.i("SUCCESS", "0");
 
 
-        Log.i("SUCCESS", "1");
+            //Creating the interperter from the model
+
+            FirebaseModelOptions options = new FirebaseModelOptions.Builder()
+                    .setRemoteModelName("building-detector").build();
+            FirebaseModelInterpreter firebaseInterpreter =
+                    FirebaseModelInterpreter.getInstance(options);
+            Log.i("SUCCESS", "0.5");
+
+            //Process
+
+            FirebaseModelInputOutputOptions inputOutputOptions =
+                    new FirebaseModelInputOutputOptions.Builder()
+                            .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 224, 224, 3})
+                            .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 4})
+                            .build();
 
 
-        //Performing Inference on Input data
+            Log.i("SUCCESS", "1");
 
-        bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
-        int batchNum = 0;
-        float[][][][] input = new float[1][224][224][3];
-        for (int x = 0; x < 224; x++) {
-            for (int y = 0; y < 224; y++) {
-                int pixel = bitmap.getPixel(x, y);
-                // Normalize channel values to [0,1.0]
-                input[batchNum][x][y][0] = Color.red(pixel) / 255.0f;
-                input[batchNum][x][y][1] = Color.green(pixel) / 255.0f;
-                input[batchNum][x][y][2] = Color.blue(pixel) / 255.0f;
+
+            //Performing Inference on Input data
+
+            bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+            int batchNum = 0;
+            float[][][][] input = new float[1][224][224][3];
+            for (int x = 0; x < 224; x++) {
+                for (int y = 0; y < 224; y++) {
+                    int pixel = bitmap.getPixel(x, y);
+                    // Normalize channel values to [0,1.0]
+                    input[batchNum][x][y][0] = Color.red(pixel) / 255.0f;
+                    input[batchNum][x][y][1] = Color.green(pixel) / 255.0f;
+                    input[batchNum][x][y][2] = Color.blue(pixel) / 255.0f;
+                }
             }
-        }
-        Log.i("SUCCESS", "2");
-        //Creating a input model
-        FirebaseModelInputs inputs = new FirebaseModelInputs.Builder()
-                .add(input)  // add() as many input arrays as your model requires
-                .build();
+            Log.i("SUCCESS", "2");
+            //Creating a input model
+            FirebaseModelInputs inputs = new FirebaseModelInputs.Builder()
+                    .add(input)  // add() as many input arrays as your model requires
+                    .build();
 
-        firebaseInterpreter.run(inputs, inputOutputOptions)
-                .addOnSuccessListener(
-                        new OnSuccessListener<FirebaseModelOutputs>() {
-                            @Override
-                            public void onSuccess(FirebaseModelOutputs result) {
-                                Log.i("SUCCESS", "4");
-                                float[][] output = result.getOutput(0);
-                                float[] probabilities = output[0];
-                                String[] labels={"Arts","Cox","Mcknighnight","Rainbow"};
+            firebaseInterpreter.run(inputs, inputOutputOptions)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<FirebaseModelOutputs>() {
+                                @Override
+                                public void onSuccess(FirebaseModelOutputs result) {
+                                    Log.i("SUCCESS", "4");
+                                    float[][] output = result.getOutput(0);
+                                    float[] probabilities = output[0];
+                                    String[] labels = {"Arts", "Cox", "Mcknighnight", "Rainbow"};
 
-                                for (int i = 0; i < probabilities.length; i++) {
-                                    try {
-                                        BufferedReader reader = new BufferedReader(
-                                                new InputStreamReader(getAssets().open("train.txt")));
-                                        String label = reader.readLine();
+                                    for (int i = 0; i < probabilities.length; i++) {
+                                        try {
+                                            BufferedReader reader = new BufferedReader(
+                                                    new InputStreamReader(getAssets().open("train.txt")));
+                                            String label = reader.readLine();
 
-                                        Log.i("MLKit", String.format("%s: %1.4f", labels[i], probabilities[i]));
-                                    } catch (Exception e) {
-                                        Log.i("MLKIT", "FAIL");
+                                            Log.i("MLKit", String.format("%s: %1.4f", labels[i], probabilities[i]));
+                                        } catch (Exception e) {
+                                            Log.i("MLKIT", "FAIL");
+                                        }
+
                                     }
 
                                 }
-
-                            }
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.i("ERROR", " FAILURE AT THE END");
-                            }
-                        });
-        Log.i("SUCCESS", "3");
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.i("ERROR", " FAILURE AT THE END");
+                                }
+                            });
+            Log.i("SUCCESS", "3");
 
 
-    }
-
-    private void goToGallery() {
-        Intent galleryIntent;
-        galleryIntent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(galleryIntent, ACTIVITY_SELECT_PICTURE);
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //Variables
-        ImageView displayedPicture = findViewById(R.id.preview_photo);
-        Uri selectedURI;
-        super.onActivityResult(requestCode, resultCode, data);
-        //If it works
-        switch (requestCode) {
-            case ACTIVITY_SELECT_PICTURE:
-                if (resultCode == Activity.RESULT_OK) {
-                    selectedURI = data.getData();
-                    Intent resultIntent = new Intent();
-                    resultIntent.setClassName("com.example.buildingpoint",
-                            "com.example.buildingpoint.ResultPage");
-                    resultIntent.putExtra("theURI", selectedURI.toString());
-                    startActivityForResult(resultIntent, ACTIVITY_RESULT);
-                }
-                break;
-            default:
-                Log.i("ERROR", "U DONE MESSED UP");
-                break;
         }
+
+        private void goToGallery () {
+            Intent galleryIntent;
+            galleryIntent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, ACTIVITY_SELECT_PICTURE);
+        }
+
+
     }
 
 
-}
